4098cc6ebc88a6f8f7c7ed5d7248bedf07b3d308
Anthony-Amgad <19p9880@eng.asu.edu.eg>
2022-05-18 20:46:52 +0200

    Maven dependencies updated

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,12 @@
 module com.example.cse338project {
     requires javafx.controls;
     requires javafx.fxml;
-    requires org.json;
-    requires jackson.databind;
-    requires jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.core;
+    requires json;
 
 
     opens com.example.cse338project to javafx.fxml;
+    opens Scrapping to com.fasterxml.jackson.databind;
     exports com.example.cse338project;
 }
